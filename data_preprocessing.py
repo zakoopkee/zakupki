5dac6f6ed7f91dcff01577dc7a86daa4c5cba5f1
Dodo888 <darkdodo888@gmail.com>
2018-01-11 19:54:21 +0500

    Train-test split

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -15,6 +15,7 @@ def preprocess_data(data):
         data['Amount'].str.replace(',', '.'), errors='coerce'
     )
     data = convert_currencies(data)
+    data['ResultClass'] = [get_result_class(x, y) for x, y in zip(data.StatusCode, data.IsWinner)]
 
     return data
 
