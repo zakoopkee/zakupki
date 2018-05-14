5b143d84661120b2191173ef1b52ba1b150b221c
p.vostretsov <p.vostretsov@skbkontur.ru>
2018-05-14 16:28:35 +0500

    gridsearch for catboost

--- feature_extractor.py
+++ feature_extractor.py
@@ -11,13 +11,20 @@ columns_drop = ['id', 'Title', 'Uri', 'PublicationDateTimeUTC', 'ProcedureDispla
                 'StatusDisplayName', 'StatusCode', 'SuppliersCount', 'IsWinner']
 
 
-def extract_features(data: pd.DataFrame, use_pca=False, title_features=300, pca_features=100, verbose=False):
-    onehot_currency = get_onehot(data, 'CurrencyCode')
-    print('oh CurrencyCode')
-    onehot_proceduredisplayname = get_onehot(data, 'ProcedureDisplayName')
-    print('oh ProcedureDisplayName')
-    onehot_ogrn = get_onehot(data, 'Ogrn1')
-    print('oh Ogrn')
+def extract_features(data: pd.DataFrame, onehot_encode=True, use_pca=False, title_features=300,
+                     pca_features=100, verbose=False):
+    if onehot_encode:
+        onehot_currency = get_onehot(data, 'CurrencyCode')
+        print('oh CurrencyCode')
+        onehot_proceduredisplayname = get_onehot(data, 'ProcedureDisplayName')
+        print('oh ProcedureDisplayName')
+        onehot_ogrn = get_onehot(data, 'Ogrn1')
+        print('oh Ogrn')
+    else:
+        cat_features = ['CurrencyCode', 'Ogrn1']
+        le_dict = {col: LabelEncoder() for col in cat_features}
+        for col in cat_features:
+            data[col] = le_dict[col].fit_transform(data[col])
 
     ngrams_title = get_ngrams(data, 'Title', n_features=title_features, ngram_range=(3, 3), analyzer='char_wb')
     print('ng Title')
@@ -26,19 +33,19 @@ def extract_features(data: pd.DataFrame, use_pca=False, title_features=300, pca_
     ngrams_orgname = get_ngrams(data, 'Name', n_features=300, ngram_range=(1, 1), analyzer='word')
     print('ng OrgName')
 
-    dfs = [data, onehot_currency, onehot_proceduredisplayname, onehot_ogrn, ngrams_title, ngrams_proceduredisplayname, ngrams_orgname]
+    dfs = (
+        [data] +
+        ([onehot_currency, onehot_proceduredisplayname, onehot_ogrn] if onehot_encode else []) +
+        [ngrams_title, ngrams_proceduredisplayname, ngrams_orgname]
+    )
     pca_ = None
     if use_pca:
         df, pca_ = pca(ngrams_title, pca_features, 'Title', verbose)
         dfs.append(df)
 
-    return pd.concat([data[['RubPrice', 'ResultClass', 'Amount']],
-                      onehot_currency,
-                      onehot_proceduredisplayname,
-                      onehot_ogrn,
-                      ngrams_title,
-                      ngrams_proceduredisplayname,
-                      ngrams_orgname], axis=1), pca_
+    return pd.concat([data[['RubPrice', 'ResultClass', 'Amount']]] +
+                     ([onehot_currency, onehot_proceduredisplayname, onehot_ogrn] if onehot_encode else [data[['CurrencyCode', 'Ogrn1']], get_label(data, 'ProcedureDisplayName')]) +
+                     [ngrams_title, ngrams_proceduredisplayname, ngrams_orgname], axis=1), pca_
 
 
 def pca(data, n_features, title, verbose):
@@ -83,3 +90,12 @@ def get_onehot(data, column):
     onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
     labels = data[column].sort_values().unique()
     return pd.DataFrame(onehot_encoded, columns=labels).add_prefix(f'OneHot_{column}_')
+
+def get_label(data, column):
+    values = data[column].astype(str).values
+    # integer encode
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(values)
+    # binary encode
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    return pd.DataFrame(integer_encoded, columns=[column])
